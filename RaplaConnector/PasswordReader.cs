1974ba522946236a6398805b7d0c18c38d622d3f
thomashoffmann.exe <thomashoffmann.exe@googlemail.com>
2011-11-30 16:47:51 +0100

    password reader

---
+++ RaplaConnector/PasswordReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace RaplaConnector
+{
+    static class PasswordReader
+    {
+        public static string ReadPassword()
+        {
+            Stack pass = new Stack();
+
+            for (ConsoleKeyInfo consKeyInfo = Console.ReadKey(true);
+              consKeyInfo.Key != ConsoleKey.Enter; consKeyInfo = Console.ReadKey(true))
+            {
+                if (consKeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    try
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        pass.Pop();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                    }
+                }
+                else
+                {
+                    Console.Write("*");
+                    pass.Push(consKeyInfo.KeyChar.ToString());
+                }
+            }
+
+            string[] password = Transform(pass.ToArray());
+            Array.Reverse(password);
+            return string.Join(string.Empty, password);
+        }
+
+        private static string[] Transform(object[] array)
+        {
+            string[] final = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                final[i] = (string)array[i];
+            }
+            return final;
+        }
+    }
+}
--- RaplaConnector/Program.cs
+++ RaplaConnector/Program.cs
@@ -18,12 +18,11 @@ namespace RaplaConnector
             Parser parser = new Parser();
             parser.printAppointments();
 
-
             Console.WriteLine("Please enter your username:");
             String user = Console.ReadLine();
 
             Console.WriteLine("Please enter your password:");
-            String password = ReadPassword();
+            String password = PasswordReader.ReadPassword();
 
             try
             {
@@ -48,50 +47,6 @@ namespace RaplaConnector
                 Console.Write("> ");
                 selection = Console.ReadKey().KeyChar.ToString();
             }
-        }
-
-        private static string ReadPassword()
-        {
-            Stack pass = new Stack();
-
-            for (ConsoleKeyInfo consKeyInfo = Console.ReadKey(true);
-              consKeyInfo.Key != ConsoleKey.Enter; consKeyInfo = Console.ReadKey(true))
-            {
-                if (consKeyInfo.Key == ConsoleKey.Backspace)
-                {
-                    try
-                    {
-                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                        Console.Write(" ");
-                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                        pass.Pop();
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
-                    }
-                }
-                else
-                {
-                    Console.Write("*");
-                    pass.Push(consKeyInfo.KeyChar.ToString());
-                }
-            }
-
-            string[] password = Transform(pass.ToArray());
-            Array.Reverse(password);
-            return string.Join(string.Empty, password);
-        }
-
-        private static string[] Transform(object[] array)
-        {
-            string[] final = new string[array.Length];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                final[i] = (string)array[i];
-            }
-            return final;
         }
     }
 }
